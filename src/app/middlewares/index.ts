d1d94dd00ec1fc4a40f327c45ddc2a471cf199bc
moneem <moneem.all.abdullah@gmail.com>
2026-04-16 21:08:08 +0600

    refactor: create middleware barrel export
    Created src/app/middlewares/index.ts with re-exports:
    - CheckAuth from checkAuth.middlewares
    - globalErrorHandler from GlobalErrorHandeler
    - validateRequest from validateRequest
    - notFoundMiddleware (default export)

---
+++ src/app/middlewares/index.ts
@@ -0,0 +1,4 @@
+export { CheckAuth } from './checkAuth.middlewares';
+export { globalErrorHandler } from './GlobalErrorHandeler';
+export { validateRequest } from './validateRequest';
+export { default as notFoundMiddleware } from './notFound';
